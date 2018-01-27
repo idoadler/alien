82710fa746a46ee3b738061a78525c884f66e534
DESKTOP-3RI9R03\ido <games@ido.wtf>
2018-01-27 18:19:02 +0200

    resolution support

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -76,4 +76,12 @@ public class GameManager : MonoBehaviour {
             NewGame();
         }
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
 }
