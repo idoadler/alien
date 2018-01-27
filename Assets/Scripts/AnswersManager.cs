9d832c6b42cc78e590532e52befdfd84ffc87485
DESKTOP-3RI9R03\ido <games@ido.wtf>
2018-01-27 11:04:56 +0200

    Initial commit

---
+++ Assets/Scripts/AnswersManager.cs
@@ -0,0 +1,52 @@
+﻿using I2.Loc;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AnswersManager : MonoBehaviour {
+    public const int SENTENCES_NUM = 5;
+    static public AnswersManager Instance;
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI[] options;
+
+    public int languageIndex = 1;
+
+    private List<string> languages;
+    private int currectAnswer;
+
+    private void Awake()
+    {
+        Instance = this;
+        languages = LocalizationManager.GetAllLanguages();
+    }
+
+    public int SetTexts(int stage)
+    {
+        int correctAnswer = Random.Range(0, options.Length);
+        int sentence = Random.Range(0, SENTENCES_NUM)+1;
+        titleText.text = LocalizationManager.GetTranslation("right" + stage + "." + sentence, false, 0, true, false, null, languages[languageIndex]);
+        titleText.isRightToLeftText = (languageIndex == 1);
+        for (int i = 0; i < options.Length; i++)
+        {
+            int lng = i;// Random.Range(0, languages.Count - 1);
+            if (lng >= languageIndex)
+            {
+                lng++;
+            }
+
+            string type = (i == correctAnswer) ? "right" : "wrong";
+
+            options[i].text = LocalizationManager.GetTranslation(type + stage + "." + sentence, false, 0, true, false, null, languages[lng]);
+
+            // if hebrew set rtl
+            options[i].isRightToLeftText = (lng == 1);
+        }
+        return correctAnswer;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject langScreen;
+    public GameObject gameScreen;
+    public GameObject winScreen;
+    public GameObject loseScreen;
+
+    private int attempts = 0;
+    private int correct =  0;
+	// Use this for initialization
+	void Start () {
+        Init();
+	}
+
+    public void Init()
+    {
+        attempts = 0;
+        langScreen.SetActive(true);
+        gameScreen.SetActive(false);
+        winScreen.SetActive(false);
+        loseScreen.SetActive(false);
+    }
+
+    public void NewGame()
+    {
+        gameScreen.SetActive(true);
+        correct = AnswersManager.Instance.SetTexts(attempts+1);
+    }
+
+    public void SetLang(int i)
+    {
+        AnswersManager.Instance.languageIndex = i;
+        langScreen.SetActive(false);
+        NewGame();
+    }
+
+    public void Check(int i)
+    {
+        attempts++;
+        if (i != correct)
+        {
+            gameScreen.SetActive(false);
+            loseScreen.SetActive(true);
+        } else if (attempts == 3)
+        {
+            gameScreen.SetActive(false);
+            winScreen.SetActive(true);
+        } else
+        {
+            NewGame();
+        }
+    }
+}
